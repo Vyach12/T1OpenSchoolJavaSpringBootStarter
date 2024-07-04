967ed624f17ea32814680fc63f36805d5e13eff2
Vyach12 <slava.gusarov.73@mail.ru>
2024-07-04 17:19:26 +0300

    [feat]: init commit

---
+++ src/test/java/openschool/java/spring_boot_starter/SpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package openschool.java.spring_boot_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
